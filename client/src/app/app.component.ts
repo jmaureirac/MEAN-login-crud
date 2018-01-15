8290ac0ced90504359a0a6b870bc8e82b15670c2
jmaureirac <jmaureira@icci.cl>
2018-01-15 02:45:50 -0300

    app component loaded, not complete

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute, Params } from '@angular/router'
+import { UserService } from './services/user.service';
+import { GLOBAL } from './services/global';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [UserService]
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit{
+   public title:string;
+   public url:string;
+
+   constructor(
+      private _route: ActivatedRoute,
+      private _router: Router,
+      private _userService: UserService
+   ){
+      this.title = 'JORGEMC';
+      this.url = GLOBAL.url;
+   }
+
+   ngOnInit(){
+      console.log('App Component Started');
+   }
 }
