0a64ffb75119973e95259fe3a717209df08744a9
jmaureirac <jmaureira@icci.cl>
2018-01-15 02:42:55 -0300

    loading login/register components, set routes and load modules

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,18 +1,38 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms'
+import { HttpClientModule } from '@angular/common/http';
+import { routing, appRoutingProviders } from './app.routing';
+import { MomentModule } from 'angular2-moment';
 
-
+//componentes
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+
+
+//servicios
+import { UserService } from './services/user.service';
 
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+   declarations: [
+      AppComponent,
+      LoginComponent,
+      RegisterComponent
+   ],
+   imports: [
+      BrowserModule,
+      routing,
+      FormsModule,
+      HttpClientModule,
+      MomentModule
+
+   ],
+   providers: [
+      appRoutingProviders,
+      UserService
+   ],
+   bootstrap: [AppComponent]
 })
 export class AppModule { }
